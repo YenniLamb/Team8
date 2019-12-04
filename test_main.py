bcbf33a88d4358c09bc89e53d90ffd9978da250f
YenniLamb <47261678+YenniLamb@users.noreply.github.com>
2019-12-03 23:51:23 -0800

    test unit 3  and update variable
    testing the model when it is not empty

--- test_main.py
+++ test_main.py
@@ -6,16 +6,29 @@ from flask_login import current_user, login_user, logout_user
 
 @pytest.fixture(scope = 'module')
 def new_user():
-  user = User(email = 'yennhilam@ymail.com' , username = 'username', password_hash = generated_password_hash('1234'))
-  return user
+    user = User(email = 'yennhilam@ymail.com' , username = 'username', password_hash = generated_password_hash('1234'))
+    return user
+
+@pytest.fixture(scope = 'module')
+def new_task():
+    task = Post(nameTitle=' Studying Finals', user_id = '1', complete = False )
+    return task
 
 # Pytest 1
-# testing validating new user yennhilam@ymail.com <--- maybe need to change 
+# testing validating new user yennhilam@ymail.com <---  maybe need to change
 def test_new_user(new_user):
-  assert new_user.email == 'yennhilam@ymail.com' 
-  assert new_user.password_hash != generate_password_hash('1234')
+    assert new_user.email == 'yennhilam@ymail.com'
+    assert new_user.password_hash != generate_password_hash('1234')
   
 # Pytest 2
-# verifying the new user
+# verifying the new user is yennhilam@ymail.com
 def test_login(new_user):
-  assert new_user.is_authenticated == True
+    assert new_user.is_authenticated == True
+  
+# Pytest 3
+# testing the name content of model when it is not empty
+def test_new_task(new_task):
+    assert new_task.nameTitle != None
+    
+
+
